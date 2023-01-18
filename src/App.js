016f9b13cce4982b976a272ff884a37c8f9ce2ed
wafabh95 <wafabenhamouda1lr1@gmail.com>
2023-01-18 14:30:31 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {increment} from './Redux/counterSlice'
+import { useDispatch,useSelector } from 'react-redux';
 function App() {
+  const count = useSelector(state=>state.counterReducer.count)
+
+  const dispatch = useDispatch()
+const handleIncr=()=>{
+  dispatch(increment())
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={()=>dispatch(handleIncr)}>+</button>
+        {count}
       </header>
     </div>
   );
---
+++ src/Redux/store.js
@@ -0,0 +1,3 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {counterReducer} from './counterSlice'
+export default configureStore({reducer:{counterReducer}})
